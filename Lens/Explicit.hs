870ed197375334cee63bcbbd73b25a27fcaaaa97
Justus Sagemüller <sagemueller@geo.uni-koeln.de>
2017-03-15 17:50:00 +0100

    Looks like this project actually makes sense: implemented the essential optics.

--- Lens/Explicit.hs
+++ Lens/Explicit.hs
@@ -8,56 +8,100 @@
 -- Portability : portable
 -- 
 
-{-# LANGUAGE TypeFamilies    #-}
-{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE TypeFamilies                 #-}
+{-# LANGUAGE TypeOperators                #-}
+{-# LANGUAGE FlexibleContexts             #-}
+{-# LANGUAGE FlexibleInstances            #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving   #-}
+{-# LANGUAGE GADTs                        #-}
+{-# LANGUAGE UnicodeSyntax                #-}
+{-# LANGUAGE Rank2Types                   #-}
 
 module Lens.Explicit where
+    
+import Prelude hiding (id, (.))
+import Control.Category
+import Data.Void
+
+newtype Accessor x y = Accessor { runAccessor :: x -> y }
+  deriving (Category)
+
+type Optic c s t a b = Accessor (c s t a b a b s t) (c s t a b s t a b)
 
 
-newtype Optic' l a s = Optic { runOptic :: l a a -> l s s }
+--  I S O S
 
-class Æther c where
-  data Light c a s α あ
+class FromIso c where
+  iso :: (s -> a) -> (b -> t) -> Optic c s t a b
 
---  G E T T E R S
+newtype IsoTrait s t a b α β σ τ = IsoTrait (τ -> σ)
 
-class Æther c => ÆGet c where
-  (^.) :: Optic' (Light c a s) a s -> s -> a
+instance FromIso IsoTrait where
+  iso g f = Accessor $ \(IsoTrait q) -> IsoTrait $ g . q . f 
 
-data Get
+type Iso s t a b = ∀ c . FromIso c => Optic c s t a b
 
-instance Æther Get where
-  newtype Light Get a s α あ = Gets { runGets :: α -> a }
-instance ÆGet Get where
-  (^.) (Optic f) = runGets . f $ Gets id
+type AnIso s t a b = Optic IsoTrait s t a b
 
---  S E T T E R S
-
-class Æther c => ÆSet c where
-  (%~) :: Optic' (Light c a s) a s -> (a -> a) -> s -> s
-
-data Set
-
-instance Æther Set where
-  newtype Light Set a s α あ = Sets { runSets :: α -> あ }
-instance ÆSet Set where
-  (%~) (Optic f) = runSets . f . Sets
-
+under :: AnIso s t a b -> (t -> s) -> b -> a
+under (Accessor f) g = case f $ IsoTrait g of IsoTrait q -> q
 
 --  L E N S E S
 
-type ÆLens c = (ÆGet c, ÆSet c)
+class FromIso c => FromLens c where
+  lens :: (s -> a) -> (s -> b -> t) -> Optic c s t a b
 
-data Lense
+type Lens s t a b = ∀ c . FromLens c => Optic c s t a b
 
-instance Æther Lense where
-  data Light Lense a s α あ = Lenss { lensGets :: α -> a, lensSets :: α -> あ }
-instance ÆGet Lense where
-  (^.) (Optic f) = lensGets . f $ Lenss id id
-instance ÆSet Lense where
-  (%~) (Optic f) = lensSets . f . Lenss id
+--  G E T T E R S
 
--- lens :: (s -> a) -> (s -> a -> s) -> Optic (Light Lense a s) a s
--- lens g s = Optic $ \(Lenss gs ss) -> Lenss _ _
+class FromLens c => FromGetter c where
+  to :: (s -> a) -> Optic c s t a b
 
+newtype GetterTrait s t a b α β σ τ = GetterTrait (α -> b)
+
+instance FromIso GetterTrait where
+  iso g _ = to g
+instance FromLens GetterTrait where
+  lens g _ = to g
+instance FromGetter GetterTrait where
+  to g = Accessor $ \(GetterTrait q) -> GetterTrait $ q . g
+
+type Getter s a = ∀ c . FromGetter c => Optic c s s a a
+
+type AGetter s a = Optic GetterTrait s s a a
+
+infixl 8 ^.
+
+(^.) :: AGetter s a -> s -> a
+(^.) (Accessor f) = case f $ GetterTrait id of GetterTrait q -> q
+
+foldMapOf :: Optic GetterTrait s t a b -> (a -> b) -> s -> b
+foldMapOf (Accessor f) g = case f $ GetterTrait g of GetterTrait q -> q
+
+--  S E T T E R S
+
+class FromLens c => FromSetter c where
+  sets :: ((a -> b) -> s -> t) -> Optic c s t a b
+
+newtype SetterTrait s t a b α β σ τ = SetterTrait (α -> β)
+
+instance FromIso SetterTrait where
+  iso g f = Accessor $ \(SetterTrait q) -> SetterTrait $ f . q . g
+instance FromLens SetterTrait where
+  lens g f = Accessor $ \(SetterTrait q) -> SetterTrait $ \s -> f s . q $ g s
+instance FromSetter SetterTrait where
+  sets f = Accessor $ \(SetterTrait q) -> SetterTrait $ f q
+
+type Setter s t a b = ∀ c . FromSetter c => Optic c s t a b
+
+type ASetter s t a b = Optic SetterTrait s t a b
+
+infixr 4 %~, .~
+
+(%~) :: ASetter s t a b -> (a -> b) -> s -> t
+Accessor f %~ m = case f $ SetterTrait m of SetterTrait q -> q
+
+(.~) :: ASetter s t a b -> b -> s -> t
+a .~ b = a %~ const b
 
