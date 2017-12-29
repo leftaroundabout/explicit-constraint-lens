9b8950d55eb383a11ecaf76646d2ab6f04d05f00
Justus Sagemüller <sagemueller@geo.uni-koeln.de>
2017-12-29 01:20:28 +0100

    Complete redesign of the underlying representation.
    Now `Optic` is basically just a category, with the different lens etc.
    flavours as GADT options.

--- Lens/Explicit.hs
+++ Lens/Explicit.hs
@@ -8,23 +8,96 @@
 -- Portability : portable
 -- 
 
+{-# LANGUAGE Rank2Types      #-}
+{-# LANGUAGE GADTs           #-}
+{-# LANGUAGE UnicodeSyntax   #-}
+
 
 module Lens.Explicit (
                      -- * Getters
-                       (^.), to, Getter, FromGetter, AGetter
+                       (^.), to, Getter, AGetter
                      -- * Setters
-                     , (%~), (.~), sets, Setter, FromSetter, ASetter
+                     , (%~), (.~), sets, Setter, ASetter
                      -- * Lenses
-                     , getset, lens, Lens, FromLens, ALens
+                     , (%%~), lens, Lens, ALens
                      -- * Prisms
-                     , matching, prism, Prism, FromPrism, APrism
+                     , matching, prism, Prism, APrism
                      -- * Isomorphisms
-                     , from, iso, Iso, FromIso, AnIso
+                     , from, iso, under, Iso, AnIso
                      -- * Traversals
-                     , traverseOf, traversed, Traversal, FromTraversal, ATraversal
-                     -- * Folds
-                     , foldrOf, foldl'Of, foldMapOf, folded, Fold, FromFold, AFoldr, AFoldl
+                     , traverseOf, traversed, Traversal, ATraversal
                      ) where
 
-import Lens.Explicit.Core
+import qualified Lens.Explicit.Core as Ж
+
+
+
+infixl 8 ^.
+
+(^.) :: s -> AGetter s a -> a
+s ^. Ж.Getter f = f s
+
+to :: (s -> a) -> Getter s a
+to = Ж.to
+
+type Getter s a = Ж.Getter s s a s
+type AGetter s a = Ж.AGetter s a
+
+
+infixr 4 %~, .~
+
+(%~) :: ASetter s t a b -> (a -> b) -> s -> t
+Ж.Setter f %~ m = f m
+
+(.~) :: ASetter s t a b -> b -> s -> t
+a .~ b = a %~ const b
+
+sets :: ((a -> b) -> s -> t) -> Setter s t a b
+sets = Ж.sets
+
+type Setter s t a b = Ж.Setter s t a b
+type ASetter s t a b = Ж.ASetter s t a b
+
+
+(%%~) :: Functor f => ALens s t a b -> (a -> f b) -> s -> f t
+(%%~) (Ж.Lens f φ) τ s = fmap (φ s) . τ $ f s
+
+lens :: (s -> a) -> (s -> b -> t) -> Lens s t a b
+lens = Ж.lens
+
+type Lens s t a b = Ж.Lens s t a b
+type ALens s t a b = Ж.ALens s t a b
+
+
+prism :: (b -> t) -> (s -> Either t a) -> Prism s t a b
+prism = Ж.prism
+
+matching :: APrism s t a b -> s -> Either t a
+matching (Ж.Prism _ f) = f
+
+type Prism s t a b = Ж.Prism s t a b
+type APrism s t a b = Ж.APrism s t a b
+
+
+under :: AnIso s t a b -> (t -> s) -> b -> a
+under (Ж.Iso f φ) g = f . g . φ
+
+from :: AnIso s t a b -> Iso b a t s
+from (Ж.Iso f φ) = iso φ f
+
+iso :: (s -> a) -> (b -> t) -> Iso s t a b
+iso = Ж.iso
+
+type Iso s t a b = Ж.Iso s t a b
+type AnIso s t a b = Ж.AnIso s t a b
+
+
+traverseOf :: Applicative f => ATraversal s t a b -> (a -> f b) -> s -> f t
+traverseOf (Ж.Traversal y) = y
+
+traversed :: (∀ f . Applicative f => (a -> f b) -> s -> f t) -> Traversal s t a b
+traversed = Ж.traversed
+
+type Traversal s t a b = Ж.Traversal s t a b
+type ATraversal s t a b = Ж.ATraversal s t a b
 
--- Lens/Explicit/Core.hs
+++ Lens/Explicit/Core.hs
@@ -8,271 +8,146 @@
 -- Portability : portable
 -- 
 
-{-# LANGUAGE TypeFamilies                 #-}
-{-# LANGUAGE TypeOperators                #-}
-{-# LANGUAGE FlexibleContexts             #-}
-{-# LANGUAGE FlexibleInstances            #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving   #-}
 {-# LANGUAGE GADTs                        #-}
 {-# LANGUAGE UnicodeSyntax                #-}
 {-# LANGUAGE Rank2Types                   #-}
-{-# LANGUAGE DeriveFunctor                #-}
 
 module Lens.Explicit.Core where
     
 import Prelude hiding (id, (.))
 import Control.Category
-import qualified Data.Foldable as Foldable
+import Control.Monad ((>=>))
+import Control.Arrow ((+++))
+import Data.Functor.Identity
 
-newtype Accessor x y = Accessor { runAccessor :: x -> y }
-  deriving (Category)
 
-type Optic (c :: * -> * -> * -> * -> *) s t a b
-        = ∀ x y . Accessor (Focus c s t a b x y) (c s t a b)
+data IsoTrait
+data GetterTrait
+data SetterTrait
+data LensTrait
+data PrismTrait
+data FoldTrait
+data TraversalTrait
 
-type AnOptic c s t a b = Accessor (Focus c s t a b a b) (c s t a b)
+type Optic c s t a b = OpticC c (s,t) (a,b)
+data OpticC c x y where
+  Equality :: OpticC c q q
+  Iso :: (s -> a) -> (b -> t) -> AnIso s t a b
+  Lens :: (s -> a) -> (s -> b -> t) -> ALens s t a b
+  Prism :: (b -> t) -> (s -> Either t a) -> APrism s t a b
+  Fold :: (∀ f . Applicative f => (a -> f r) -> s -> f r) -> Optic FoldTrait s b a r
+  Traversal :: (∀ f . Applicative f => (a -> f b) -> s -> f t)
+                             -> ATraversal s t a b
+  Getter :: (s -> a) -> Optic GetterTrait s t a b
+  Setter :: ((a -> b) -> s -> t) -> ASetter s t a b
 
-class Æther (c :: * -> * -> * -> * -> *) where
-  data Focus c s t a b :: * -> * -> *
-  liftOptic :: AnOptic c s t a b -> Optic (Focus c x y) s t a b
+instance Category (OpticC c) where
+  id = Equality
+  Equality . f = f
+  f . Equality = f
+  Iso f φ . Iso g γ = Iso (f . g) (γ . φ)
+  Lens f φ . Lens g γ = Lens (f . g) (\s b -> γ s $ φ (g s) b)
+  Prism φ f . Prism γ g = Prism (γ . φ) (g >=> (γ+++id) . f)
+                                                -- Left ξ -> Left $ γ ξ
+                                                -- Right υ -> Right υ)
+  Getter f . Getter g = Getter (f . g)
+  Setter σ . Setter s = Setter $ s . σ
+  
 
---  I S O S
+-- ⣿⢠⡤⠤⢀⣤⢤⣄⢠⡤⠤
+-- ⣿⢈⣛⡷⠸⣧⣠⡿⢈⣛⡷
 
-class Æther c => FromIso c where
-  iso :: (s -> a) -> (b -> t) -> Optic c s t a b
-
+type AnIso s t a b = Optic IsoTrait s t a b
 type Iso s t a b = ∀ c . FromIso c => Optic c s t a b
 
-data IsoTrait s t a b = IsoTrait (s -> a) (b -> t)
-instance Æther IsoTrait where
-  data Focus IsoTrait s t a b x y = IsoFocus (a -> a, t -> t)
-  liftOptic (Accessor α) = Accessor $ \(IsoFocus φ) -> case α _ of
-                             IsoFocus (f,g) -> _
-
+class FromIso c where
+  iso :: (s -> a) -> (b -> t) -> Optic c s t a b
 instance FromIso IsoTrait where
-  iso g f = Accessor $ \(IsoFocus (p,q)) -> IsoTrait (p . g) (q . f)
+  iso = Iso
+instance FromIso GetterTrait where
+  iso f _ = Getter f
+instance FromIso LensTrait where
+  iso f φ = Lens f (\_ b -> φ b)
+instance FromIso PrismTrait where
+  iso f φ = Prism φ (Right . f)
+instance FromIso SetterTrait where
+  iso f φ = Setter $ \u -> φ . u . f
+instance FromIso FoldTrait where
+  iso f _ = Fold (\t -> t . f)
+instance FromIso TraversalTrait where
+  iso f φ = Traversal (\t -> fmap φ . t . f)
 
-type AnIso s t a b = AnOptic IsoTrait s t a b
 
-under :: AnIso s t a b -> (t -> s) -> b -> a
-under (Accessor f) g = case f $ IsoFocus (id,id) of IsoTrait p q -> p . g . q
+-- ⣿⠀⠀⣠⠤⣄⢠⡤⢤⡀⣤⠤⠄⣠⠤⣄⢠⡤⠤
+-- ⣿⣀⣀⢿⣒⣛⢸⡇⢸⡇⣙⣻⠆⢿⣒⣛⢈⣛⡷
 
-from :: AnIso s t a b -> Iso b a t s
-from (Accessor i) = case i $ IsoFocus (id,id) of IsoTrait d b -> iso b d
-
---  L E N S E S
+type ALens s t a b = Optic LensTrait s t a b
+type Lens s t a b = ∀ c . FromLens c => Optic c s t a b
 
 class FromIso c => FromLens c where
   lens :: (s -> a) -> (s -> b -> t) -> Optic c s t a b
-
-type Lens s t a b = ∀ c . FromLens c => Optic c s t a b
-
-data LensTrait s t a b = LensTrait a (b->t)
-instance Æther LensTrait where
-  data Focus LensTrait s t a b x y = LensFocus (s, b->b)
-
-instance FromIso LensTrait where
-  iso g f = Accessor $ \(LensFocus (s,i)) -> LensTrait (g s) (f . i)
+instance FromLens GetterTrait where
+  lens f _ = Getter f
 instance FromLens LensTrait where
-  lens g f = Accessor $ \(LensFocus (s,i)) -> LensTrait (g s) (f s . i)
+  lens = Lens
+instance FromLens TraversalTrait where
+  lens f φ = Traversal (\τ s -> fmap (φ s) . τ $ f s)
+instance FromLens SetterTrait where
+  lens f φ = Setter (\τ s -> φ s . τ $ f s)
 
-type ALens s t a b = AnOptic LensTrait s t a b
 
-getset :: ALens s t a b -> s -> (a, b->t)
-getset (Accessor f) s = case f $ LensFocus (s,id) of LensTrait a m -> (a, m)
+-- ⣿⢉⣿⢠⣄⡄⣭⢠⡤⠤⢠⡤⢤⡤⢤⡄⣤⠤⠄
+-- ⣿⠉⠁⢸⡇⠀⣿⢈⣛⡷⢸⡇⢸⡇⢸⡇⣙⣻⠆
 
---  P R I S M S
+type APrism s t a b = Optic PrismTrait s t a b
+type Prism s t a b = ∀ c . FromPrism c => Optic c s t a b
 
 class FromIso c => FromPrism c where
   prism :: (b -> t) -> (s -> Either t a) -> Optic c s t a b
-
-type Prism s t a b = ∀ c . FromPrism c => Optic c s t a b
-
-data PrismTrait s t a b = PrismRemixed t
-                        | PrismFiltered a
-instance Æther PrismTrait where
-  data Focus PrismTrait s t a b x y = PrismFocus (Either b s)
-
-instance FromIso PrismTrait where
-  iso g f = Accessor $ \p -> case p of
-                PrismFocus (Left b) -> PrismRemixed $ f b
-                PrismFocus (Right s) -> PrismFiltered $ g s
 instance FromPrism PrismTrait where
-  prism f g = Accessor $ \p -> case p of
-                PrismFocus (Left b) -> PrismRemixed $ f b
-                PrismFocus (Right s) -> case g s of
-                    Left t -> PrismRemixed t
-                    Right a -> PrismFiltered a
+  prism = Prism
+instance FromPrism TraversalTrait where
+  prism φ f = Traversal (\τ -> either pure (fmap φ . τ) . f)
+instance FromPrism SetterTrait where
+  prism φ f = Setter (\τ -> either id (φ . τ) . f)
 
-type APrism s t a b = AnOptic PrismTrait s t a b
 
-matching :: APrism s t a b -> s -> Either t a
-matching (Accessor f) s = case f . PrismFocus $ Right s of
-                           PrismFiltered a -> Right a
-                           PrismRemixed t -> Left t
+-- ⣴⠋⠉⠁⣠⠤⣄⢼⡧⠠⣿⢄⡤⢤⡀⣤⣠⢠⡤⠤
+-- ⠻⣄⣸⡇⢿⣒⣛⢸⣇⠀⣿⡸⣗⣚⡃⣿⠀⢈⣛⡷
 
-withPrism :: APrism s t a b -> ((b -> t) -> (s -> Either t a) -> r) -> r
-withPrism p cont = uncurry cont $ disassemblePrism p
-
-disassemblePrism :: APrism s t a b -> (b -> t, s -> Either t a)
-disassemblePrism (Accessor f)
-     = ( \b -> case f . PrismFocus $ Left b of
-                   PrismRemixed t -> t
-                   PrismFiltered _ -> prismError "disassemblePrism"
-       , \s -> case f . PrismFocus $ Right s of
-                   PrismRemixed t -> Left t
-                   PrismFiltered a -> Right a )
-
-prismError :: String -> a
-prismError f = error $ f ++ ": Prism extracts value where none can be."
-
---  T R A V E R S A L S
-
-class (FromLens c, FromPrism c) => FromTraversal c where
-  traversed :: Traversable f => Optic c (f a) (f b) a b
-
-type Traversal s t a b = ∀ c . FromTraversal c => Optic c s t a b
-
-newtype TraversalTrait m s t a b = TraversalTrait (s -> m t)
-instance Æther (TraversalTrait m) where
-  data Focus (TraversalTrait m) s t a b x y = TraversalFocus (a -> m b)
-
-instance Functor m => FromIso (TraversalTrait m) where
-  iso g f = Accessor $ \(TraversalFocus q) -> TraversalTrait $ fmap f . q . g
-instance Functor m => FromLens (TraversalTrait m) where
-  lens g f = Accessor $ \(TraversalFocus q) -> TraversalTrait $ \s -> fmap (f s) . q $ g s
-instance Applicative m => FromPrism (TraversalTrait m) where
-  prism f g = Accessor $ \(TraversalFocus q) -> TraversalTrait $ \s -> case g s of
-                                                     Left t -> pure t
-                                                     Right a -> fmap f $ q a
-instance Applicative m => FromTraversal (TraversalTrait m) where
-  traversed = Accessor $ \(TraversalFocus q)
-                -> TraversalTrait $ traverse q
-
-type ATraversal m s t a b = AnOptic (TraversalTrait m) s t a b
-
-data StateL s x = StateL (s -> (s,x)) deriving (Functor)
-instance Applicative (StateL s) where
-  pure = StateL . flip (,)
-  StateL f<*>StateL g = StateL $ \s -> let (s',μ) = f s in fmap μ $ g s'
-
-mapAccumLOf :: ATraversal (StateL acc) s t a b
-                   -> (acc -> a -> (acc,b)) -> acc -> s -> (acc,t)
-mapAccumLOf (Accessor y) f = case y . TraversalFocus $ StateL . flip f of
-                               TraversalTrait w -> \acc s -> case w s of
-                                        StateL p -> p acc
-
-traverseOf :: ATraversal f s t a b -> (a -> f b) -> s -> f t
-traverseOf (Accessor y) f = case y $ TraversalFocus f of
-                               TraversalTrait w -> w
-
---  G E T T E R S
+type AGetter s a = Optic GetterTrait s s a a
+type Getter s t a b = ∀ c . FromGetter c => Optic c s t a b
 
 class FromLens c => FromGetter c where
-  to :: (s -> a) -> Optic c s t a b
-
-type Getter s a = ∀ c . FromGetter c => Optic c s s a a
-
-newtype GetterTrait s t a b = GetterTrait a
-instance Æther GetterTrait where
-  data Focus GetterTrait s t a b x y = GetterFocus s
-
-instance FromIso GetterTrait where
-  iso g _ = Accessor $ \(GetterFocus q) -> GetterTrait $ g q
-instance FromLens GetterTrait where
-  lens g _ = Accessor $ \(GetterFocus q) -> GetterTrait $ g q
+  to :: (s -> a) -> Optic c s t a t
 instance FromGetter GetterTrait where
-  to g = Accessor $ \(GetterFocus q) -> GetterTrait $ g q
+  to = Getter
+instance FromGetter TraversalTrait where
+  to f = Traversal (\t -> t . f)
 
-type AGetter s a = AnOptic GetterTrait s s a a
 
-infixl 8 ^.
 
-(^.) :: s -> AGetter s a -> a
-s ^. Accessor f = case f $ GetterFocus s of GetterTrait a -> a
+-- ⠉⢹⡏⠉⣤⣠⢠⢤⡀⢤⡀⢠⢄⡤⢤⡀⣤⣠⢠⡤⠤⠀⡤⣄⠀⣿⢠⡤⠤
+-- ⠀⢸⡇⠀⣿⠀⢿⡹⣇⠈⣷⡎⠸⣗⣚⡃⣿⠀⢈⣛⡷⠸⣏⢿⡀⣿⢈⣛⡷
 
---  F O L D S
+type ATraversal s t a b = Optic TraversalTrait s t a b
+type Traversal s t a b = ∀ c . FromTraversal c => Optic c s t a b
 
-class FromGetter c => FromFold c where
-  folded :: Foldable f => Optic c (f a) t a b
+class (FromLens c, FromPrism c) => FromTraversal c where
+  traversed :: (∀ f . Applicative f => (a -> f b) -> s -> f t) -> Optic c s t a b
+instance FromTraversal TraversalTrait where
+  traversed = Traversal
+instance FromTraversal SetterTrait where
+  traversed θ = Setter (\f -> runIdentity . θ (Identity . f))
 
-type Fold s t a b = ∀ c . FromFold c => Optic c s t a b
 
-newtype FoldlTrait r s t a b = FoldlTrait (r -> s -> r)
-instance Æther (FoldlTrait r) where
-  data Focus (FoldlTrait r) s t a b x y = FoldlFocus (r -> a -> r)
+-- ⣾⣍⠁⢀⡤⢤⡠⣿⠄⢼⡧⣠⠤⣄⢠⣄⡄⣤⠤⠄
+-- ⣀⣉⡿⠸⣗⣚⡃⣿⡀⢸⣇⢿⣒⣛⢸⡇⠀⣙⣻⠆
 
-instance FromIso (FoldlTrait r) where
-  iso g _ = to g
-instance FromLens (FoldlTrait r) where
-  lens g _ = to g
-instance FromGetter (FoldlTrait r) where
-  to g = Accessor $ \(FoldlFocus q) -> FoldlTrait $ \b -> q b . g
-instance FromFold (FoldlTrait r) where
-  folded = Accessor $ \(FoldlFocus q) -> FoldlTrait $ Foldable.foldl' q
-
-type AFoldl r s t a b = AnOptic (FoldlTrait r) s t a b
-
-foldl'Of :: AFoldl r s t a b -> (r -> a -> r) -> r -> s -> r
-foldl'Of (Accessor f) y = case f $ FoldlFocus y of FoldlTrait w -> w
-
-newtype FoldrTrait r s t a b = FoldrTrait (s -> r -> r)
-instance Æther (FoldrTrait r) where
-  data Focus (FoldrTrait r) s t a b x y = FoldrFocus (a -> r -> r)
-
-instance FromIso (FoldrTrait r) where
-  iso g _ = to g
-instance FromLens (FoldrTrait r) where
-  lens g _ = to g
-instance FromGetter (FoldrTrait r) where
-  to g = Accessor $ \(FoldrFocus q) -> FoldrTrait $ q . g
-instance FromFold (FoldrTrait r) where
-  folded = Accessor $ \(FoldrFocus q) -> FoldrTrait . flip $ Foldable.foldr q
-
-type AFoldr r s t a b = AnOptic (FoldrTrait r) s t a b
-
-foldrOf :: AFoldr r s t a b -> (a -> r -> r) -> r -> s -> r
-foldrOf (Accessor f) y = case f $ FoldrFocus y of FoldrTrait w -> flip w
-
-foldMapOf :: Monoid r => AFoldr r s t a b -> (a -> r) -> s -> r
-foldMapOf f g = foldrOf f (mappend . g) mempty
-
---  S E T T E R S
+type ASetter s t a b = Optic SetterTrait s t a b
+type Setter s t a b = ∀ c . FromSetter c => Optic c s t a b
 
 class FromTraversal c => FromSetter c where
   sets :: ((a -> b) -> s -> t) -> Optic c s t a b
-
-type Setter s t a b = ∀ c . FromSetter c => Optic c s t a b
-
-newtype SetterTrait s t a b = SetterTrait (s -> t)
-instance Æther SetterTrait where
-  data Focus SetterTrait s t a b x y = SetterFocus (a -> b)
-
-instance FromIso SetterTrait where
-  iso g f = Accessor $ \(SetterFocus q) -> SetterTrait $ f . q . g
-instance FromLens SetterTrait where
-  lens g f = Accessor $ \(SetterFocus q) -> SetterTrait $ \s -> f s . q $ g s
-instance FromPrism SetterTrait where
-  prism g f = Accessor $ \(SetterFocus q) -> SetterTrait $ \s ->
-                 case f s of Left t -> t
-                             Right a -> g $ q a
-instance FromTraversal SetterTrait where
-  traversed = Accessor $ \(SetterFocus q) -> SetterTrait $ fmap q
 instance FromSetter SetterTrait where
-  sets f = Accessor $ \(SetterFocus q) -> SetterTrait $ f q
-
-type ASetter s t a b = AnOptic SetterTrait s t a b
-
-infixr 4 %~, .~
-
-(%~) :: ASetter s t a b -> (a -> b) -> s -> t
-Accessor f %~ m = case f $ SetterFocus m of SetterTrait q -> q
-
-(.~) :: ASetter s t a b -> b -> s -> t
-a .~ b = a %~ const b
-
-
-
-
-instance Æther c => Æther (Focus c x y) where
-  data Focus (Focus c x y) s t a b m n = Focus² (Focus 
+  sets = Setter
